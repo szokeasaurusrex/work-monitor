1896b3427ccc88f5ed8182ab36ea52411b8d507b
szokeasaurusrex <szokeasaurusrex@users.noreply.github.com>
2014-12-21 10:10:39 -0500

    Progress bar added

--- js.js
+++ js.js
@@ -1,10 +1,12 @@
 function Time() {
     var date = new Date();
-    this.hours = 0;
+    this.hours = 1;
     this.minutes = 0;
-    this.seconds = 5;
+    this.seconds = 0;
     this.countDown = true;
     this.lastReset = toDays(date.getTime());
+    this.absSeconds = toAbsSecs(this.hours, this.minutes, this.seconds);
+    this.maxSeconds = this.absSeconds;
 }
 
 function n(n){
@@ -23,33 +25,22 @@ function reset() {
 }
 
 function count() {
-    if (time.countDown === true) {
-        if (time.minutes == 0 && time.seconds == 0) {
-            time.hours--;
-            time.minutes = 59;
-            time.seconds = 59;
-        } else if (time.seconds == 0) {
-            time.minutes--;
-            time.seconds = 59;
-        } else {
-            time.seconds--;
-        }
-        remaining.innerHTML = n(time.hours) + ':' + n(time.minutes) + ':' + n(time.seconds);
-    } else {
-        if (time.minutes == 59 && time.seconds == 59) {
-            time.hours++;
-            time.minutes = 0;
-            time.seconds = 0;
-        } else if (time.seconds == 59) {
-            time.minutes++;
-            time.seconds = 0;
-        } else {
-            time.seconds += 1;
-        }
-        remaining.innerHTML = '-' + n(time.hours) + ':' + n(time.minutes) + ':' + n(time.seconds);
+    function newTime() {
+        var hrsMinsSecs = toHrsMinsSecs(time.absSeconds);
+        time.hours = hrsMinsSecs[0];
+        time.minutes = hrsMinsSecs[1];
+        time.seconds = hrsMinsSecs[2];
     }
+    if (time.countDown === true) {
+        time.absSeconds--;
+        newTime();
+    } else {
+        time.absSeconds++;
+        newTime();
+    }
+    updateBar();
     localStorage.time = JSON.stringify(time);
-    if (time.hours <= 0 && time.minutes <= 0 && time.seconds <= 0 && time.countDown === true) {
+    if (time.absSeconds <= 0 && time.countDown === true) {
         body.style.background = '#CC297A';
         var cont = confirm('You are out of time. Working for an extended amount of time could have unexpected consequences. Are you sure you want to continue working?');
         if (cont === true) {
@@ -96,11 +87,44 @@ function toDays(milliseconds) {
     return Math.floor(milliseconds / 86400000);
 }
 
+function toHrsMinsSecs(seconds) {
+    hours = Math.floor(seconds / 3600);
+    seconds -= hours * 3600;
+    minutes = Math.floor(seconds / 60);
+    seconds -= minutes * 60;
+    return [hours, minutes, seconds];
+}
+
+function toAbsSecs(hours, minutes, seconds) {
+    return hours * 3600 + minutes * 60 + seconds;
+}
+
+function updateBar() {
+    ctx.clearRect(0, 0, cwidth, cheight);
+    if (time.countDown === true) {
+        divide = time.absSeconds / time.maxSeconds * cwidth;
+        ctx.fillStyle = 'green';
+        ctx.fillRect(0, 0, divide, cheight);
+        ctx.fillStyle = 'red';
+        ctx.fillRect(divide, 0, cwidth - divide, cheight);
+    } else {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(0, 0, cwidth, cheight);
+    }
+    ctx.fillStyle = 'white';
+    if (time.countDown === true) ctx.fillText(n(time.hours) + ':' + n(time.minutes) + ':' + n(time.seconds), cwidth / 2, cheight / 2);
+    else ctx.fillText('-' + n(time.hours) + ':' + n(time.minutes) + ':' + n(time.seconds), cwidth / 2, cheight / 2);
+}
+
 var remaining = document.getElementById('remaining');
 var button = document.getElementById('work');
 var body = document.getElementById('body');
 //var reset_button = document.getElementById('reset');
 var interval, working;
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var cwidth = 294;
+var cheight = 32;
 
 working = false;
 button.onclick = function () {start();};
@@ -112,7 +136,7 @@ button.onclick = function () {start();};
 if (!(localStorage.time)) var time = reset();
 else {
     var time = JSON.parse(localStorage.time);
-    if (time.hours <= 0 && time.minutes <= 0 && time.seconds <= 0 || time.countDown === false) {
+    if (time.absSeconds <= 0 || time.countDown === false) {
         body.style.background = '#CC297A';
     }
     var date = new Date();
@@ -121,6 +145,8 @@ else {
     }
 }
 
+ctx.textAlign = 'center';
+ctx.textBaseline = 'middle';
+ctx.font = '20px Sans';
 
-if (time.countDown === true) remaining.innerHTML = n(time.hours) + ':' + n(time.minutes) + ':' + n(time.seconds);
-else remaining.innerHTML = '-' + n(time.hours) + ':' + n(time.minutes) + ':' + n(time.seconds);
+updateBar();
